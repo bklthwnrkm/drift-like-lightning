1aad32adc473bd773563965ce06b928e1b90ac7a
hwatanabe <tufs.h.watanabe@gmail.com>
2023-03-28 18:29:51 +0900

    introduced tailwind css

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Drift like lightning⚡</span>,
+  logo: <span>⛅Drift like lightning⚡</span>,
   project: {
     link: "https://github.com/brkwtnh",
   },
