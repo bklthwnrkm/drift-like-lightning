bae6fe500f2fbc6e285a441b7ad41982acd6f9ca
hwatanabe <tufs.h.watanabe@gmail.com>
2023-03-29 13:38:54 +0900

    Created themeSwitch.tsx and hooks/useThemeOnMounted.ts, and then replaced the default theme switch with it

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -5,20 +5,21 @@ import MoonIcon from "@/data/moon.svg";
 const ThemeSwitch = () => {
   const { mounted, theme, setTheme, resolvedTheme } = useThemeOnMounted();
   return (
-    <button
-      aria-label="Toggle between dark and light mode"
-      onClick={() =>
-        setTheme(
-          theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
-        )
-      }
-    >
-      {mounted && (theme === "dark" || resolvedTheme === "dark") ? (
-        <MoonIcon />
-      ) : (
-        <SunIcon />
-      )}
-    </button>
+    <SunIcon />
+    // <button
+    //   aria-label="Toggle between dark and light mode"
+    //   onClick={() =>
+    //     setTheme(
+    //       theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
+    //     )
+    //   }
+    // >
+    //   {mounted && (theme === "dark" || resolvedTheme === "dark") ? (
+    //     <MoonIcon />
+    //   ) : (
+    //     <SunIcon />
+    //   )}
+    // </button>
   );
 };
 
