3a44abab51d8d85e46dae943c2fe37084c5c8559
hwatanabe <tufs.h.watanabe@gmail.com>
2023-04-03 17:26:08 +0900

    Tweaked theme.config.tsx on head, and useNextSeoProps.

--- components/head.tsx
+++ components/head.tsx
@@ -1,9 +1,7 @@
-import { Head } from "next/document";
-
 const CustomHead = () => {
   return (
-    <Head>
-      <link rel="manifest" href="/manifest.json" />
+    <>
+      {/* <link rel="manifest" href="/manifest.json" />
       <link rel="apple-touch-icon" href="/icon.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -11,8 +9,15 @@ const CustomHead = () => {
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-TileColor" content="#2B5797" />
       <meta name="msapplication-tap-highlight" content="no" />
-      <meta name="theme-color" content="#fff" />
-    </Head>
+      <meta name="theme-color" content="#fff" /> */}
+
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Drift like lightning" /> */}
+      <meta
+        property="og:description"
+        content="This is a website run by Hikari"
+      />
+    </>
   );
 };
 
--- theme.config.tsx
+++ theme.config.tsx
@@ -7,8 +7,9 @@ import CustomHead from "@/components/head";
 const config: DocsThemeConfig = {
   head: (
     <>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon.png"></link>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Drift like lightning" />
       <meta
         property="og:description"
         content="This is a website run by Hikari"
